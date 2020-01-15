73749bc7d2cfd554db22a4a923316d896fc525fd
RohitTh69 <abhishekrawat5743@gmail.com>
2020-01-15 23:40:51 +0530

    Responsive Home Page upto 900px.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+document.getElementById("myNav").style.width = "0%";
+}
+
 function contentPanel(evt, contentId) {
     
     let i, tabcontent, tablinks, afterImage;
